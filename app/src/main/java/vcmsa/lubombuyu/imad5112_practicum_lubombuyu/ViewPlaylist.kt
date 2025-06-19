3bc507fb4e453966108afb92215f8d104fea7fda
LUBOMBUYU <lubombuyu@gmail.com>
2025-06-19 10:04:37 +0200

    Second user interface

---
+++ app/src/main/java/vcmsa/lubombuyu/imad5112_practicum_lubombuyu/ViewPlaylist.kt
@@ -0,0 +1,20 @@
+package vcmsa.lubombuyu.imad5112_practicum_lubombuyu
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ViewPlaylist : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_view_playlist)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
